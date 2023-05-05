52f132bfb93409d1b14bdc74baab05eb189a5f17
AJHARUL ISLAM AUNIK <AJAUNI01@WSC.EDU>
2023-05-04 22:59:36 -0500

    cors-has-been-added-to connect-the-server-side, and client side

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+// cors to connect server side, and the client side
+var cors = require('cors')
+app.use(cors())
 
 // get all chefs' data
 const allChefs = require('./data/chefRecipe.json')
